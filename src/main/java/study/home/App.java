b15d9cc471926e0da1242b0e0dc432515fef5cc1
Jake2Finn <jake.fin@rambler.ru>
2020-06-05 16:48:36 +0300

    add first part

---
+++ src/main/java/study/home/App.java
@@ -0,0 +1,14 @@
+package study.home;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name = "Hello, " + args[0];
+        System.out.println(name);
+    }
+}
